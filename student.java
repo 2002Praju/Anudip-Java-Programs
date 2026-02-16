3dfbb8abc12204f9875c7fc4a5272ba76b86f857
Prajwal Vyavahare <122688935+2002Praju@users.noreply.github.com>
2026-02-16 14:22:35 +0000

    default constructor called

---
+++ student.java
@@ -0,0 +1,10 @@
+public class student {
+    student(){
+        System.out.println("This is a default constructor.");
+    }
+    
+    
+    public static void main(String[] args) {
+        default_constructor person1 = new default_constructor();
+    }
+}
